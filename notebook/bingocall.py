334c7f9af363003e2d97bb8cd36e137af46cb7e3
jetlin <jetlin0818@gmail.com>
2018-12-16 23:04:52 +0800

    chapter5 update

--- notebook/bingocall.py
+++ notebook/bingocall.py
@@ -2,19 +2,20 @@
 import random
 
 class BingoCage:
+    """BingoCage is a class trying callable, it is replace operate ()"""
     def __init__(self, items):
         # items, any iterable
         self._items = list(items)
         random.shuffle(self._items)  #乱序
-        
+
     def pick(self):
         try:
             return self._items.pop()
         except IndexError:
             raise LookupError('pick form empty BingoCage')
-    
+
     def __call__(self):  #callable, shortcut to bingo.pick()
         return self.pick()
 
-bingo = BingoCage(range(3))
-bingo.pick()
+Bingo = BingoCage(range(3))
+Bingo.pick()
