a2387ee4bc0188367aff96a94288cfcbee0c8e93
林衡 <linheng@ZBMAC-C02WF2KRH.local>
2018-12-19 19:02:44 +0800

    chapter7 added

---
+++ notebook/strategy2.py
@@ -0,0 +1,82 @@
+#Decorator-enhanced Strategy pattern
+from collections import namedtuple
+
+Customer = namedtuple('Customer', 'name fidelity')
+promos = []
+
+def promo(promo_func):
+    promos.append(promo_func)
+    return promos
+
+class LineItem:
+    
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+        
+    def total(self):
+        return self.price * self.quantity
+    
+class Order: #the Context
+    
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        self.cart = list(cart)
+        self.promotion = promotion
+        
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.cart)
+        return self.__total
+    
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion(self)
+        return self.total() - discount
+    
+    def __repr__(self):
+        fmt = '<Order total: {:.2f} due: {:.2f}>'
+        return fmt.format(self.total(), self.due())
+    
+@promo
+def fidelity(order):
+    """5% discount for customers with 1000 or more fidelity points"""
+    return order.total() * .05 if order.customer.fidelity >= 1000 else 0
+
+@promo
+def bulk_item(order):
+    """10% discount for each LineItem with 20"""
+    discount = 0 
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * .1
+    return discount
+
+@promo
+def large_order(order):
+    """7% discount"""
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * .07
+    return 0
+
+def best_promo(order):
+    """best promo"""
+    return max(promo(order) for promo in promos)
+
+if __name__ == '__main__':
+    joe = Customer('John Doe', 0)
+    ann = Customer('Ann Smith', 1100)
+
+    cart = [LineItem('banana', 4, .5),
+           LineItem('apple', 10, 1.5),
+           LineItem('watermellon', 5, 5.0)]
+    long_order = [LineItem(str(item_code), 1, 1.0) for item_code in range(10)]
+    banana_cart = [LineItem('banana', 30, .5),
+                  LineItem('apple', 10, 1.5)]
+
+    print(Order(joe, long_order, best_promo))
+    print(Order(ann, banana_cart, best_promo))
