dc7f6511be9e3b1beb0fe422eb27ee336a00e11c
林衡 <linheng@ZBMAC-C02WF2KRH.local>
2018-12-24 18:20:43 +0800

    chapter9 update

---
+++ notebook/vector2d_v1.py
@@ -0,0 +1,47 @@
+# @classmethod
+# vector2d_v1.py
+from array import array
+import math
+
+class Vector2d:
+    typecode = 'd'
+    
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+        
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+    
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{}({!r}, {!r})'.format(class_name, *self)
+    
+    def __str__(self):
+        return str(tuple(self))
+    
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) +
+               bytes(array(self.typecode, self)))
+    
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+    
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+    
+    def __bool__(self):
+        return bool(abs(self))
+    
+    def angle(self):
+        return math.atan2(self.y, self.x)
+    
+    def __format__(self, fmt_spec=''):
+        components = (format(c, fmt_spec) for c in self)
+        return '({}, {})'.format(*components)
+    
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:].cast(typecode))
+        return cls(*memv)
