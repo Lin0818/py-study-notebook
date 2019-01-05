2de9676b2b400de9e0e3d8e8e6c4980fbb59c5a1
林衡 <jetlin0818@gmail.com>
2019-01-05 15:34:13 +0800

    chapter15 update

---
+++ notebook/mirror_gen.py
@@ -0,0 +1,18 @@
+# mirror_gen.py
+import contextlib
+
+@contextlib.contextmanager
+def looking_glass():
+    import sys
+    original_write = sys.stdout.write
+    
+    def reverse_write(text):
+        original_write(text[::-1])
+        
+    sys.stdout.write = reverse_write
+    yield 'JABBERWOCKY'
+    sys.stdout.write = original_write
+
+with looking_glass() as what:
+    print('Alice, kitty and Snowdrop')
+    print(what)
---
+++ notebook/mirror_gen_exc.py
@@ -0,0 +1,25 @@
+# mirror_gen_exc.py
+import contextlib
+
+@contextlib.contextmanager
+def looking_glass():
+    import sys
+    original_write = sys.stdout.write
+    
+    def reverse_write(text):
+        original_write(text[::-1])
+        
+    sys.stdout.write = reverse_write
+    msg = ''
+    try:
+        yield 'JABBERWOCKY'
+    except ZeroDivisionError:
+        msg = 'Please DO NOT divide by zero!'
+    finally:
+        sys.stdout.write = original_write
+        if msg:
+            print(msg)
+
+with looking_glass() as what:
+    print('Alice, kitty and Snowdrop')
+    print(what)
